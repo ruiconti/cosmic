c1d6fcfe24696fb80d21a0c56805a65685185d63
Rui Conti <ruiconti@gmail.com>
2020-04-13 17:32:11 -0300

    Sends commands through message bus

--- src/allocation/entrypoints/app.py
+++ src/allocation/entrypoints/app.py
@@ -1,50 +1,46 @@
 from flask import Flask, jsonify, request
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-from allocation import config
-from allocation.domain import model
-from allocation.service import services, unit_of_work
+from allocation.domain import commands
+from allocation.service import handlers, unit_of_work, message_bus
 from allocation.adapters import orm
-from allocation.adapters import repository
 
 orm.start_mappers()
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET"])  # type: ignore
 def default() -> tuple:
     return jsonify({"status": "on-line"}), 200
 
 
-@app.route("/allocate", methods=["POST"])
+@app.route("/allocate", methods=["POST"])  # type: ignore
 def allocate_endpoint() -> tuple:
     # batches = repository.SqlAlchemyRepository(session).list()
     # line = model.OrderLine()
-    uow = unit_of_work.SqlAlchemyUnitOfWork()
     try:
-        batchref = services.allocate(
-            request.json["order_id"],
-            request.json["sku"],
-            request.json["qty"],
-            uow=uow,
+        command = commands.Allocate(
+            order_id=request.json["order_id"],
+            sku=request.json["sku"],
+            qty=request.json["qty"],
         )
-    except (services.InvalidSku, model.OutOfStock) as ex:
+        uow = unit_of_work.SqlAlchemyUnitOfWork()
+        message_bus.handle(command, uow)
+    except handlers.InvalidSku as ex:
         return jsonify({"message": ex.message}), 400
-    return jsonify({"batchref": batchref}), 201
+    return jsonify({"status": "ok"}), 202
 
 
-@app.route("/batch", methods=["POST"])
+@app.route("/batch", methods=["POST"])  # type: ignore
 def post_endpoint() -> tuple:
     # session = get_session()
     # repo = repository.SqlAlchemyRepository(session)
 
-    uow = unit_of_work.SqlAlchemyUnitOfWork()
-    services.add_batch(
+    command = commands.CreateBatch(
         ref=request.json["ref"],
         sku=request.json["sku"],
         qty=request.json["qty"],
         eta=request.json["eta"],
-        uow=uow,
     )
+    uow = unit_of_work.SqlAlchemyUnitOfWork()
+    message_bus.handle(command, uow)
     return jsonify({"status": "ok"}), 201
